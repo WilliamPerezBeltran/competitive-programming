c508d8d825ee21aae96c019cb60a26e7d624203e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-09-11 13:51:57 -0500

    add sudoku

---
+++ sudoku_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+def es_valido_sudoku(tablero):
+    for fila in tablero:
+        if sorted(fila) != list(range(1, 10)):
+            return False
+    for col in range(9):
+        if sorted([tablero[f][col] for f in range(9)]) != list(range(1, 10)):
+            return False
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            subcuadro = [tablero[x][y] for x in range(i, i+3) for y in range(j, j+3)]
+            if sorted(subcuadro) != list(range(1, 10)):
+                return False
+    return True
+
+class TestSudoku(unittest.TestCase):
+    def test_sudoku_valido(self):
+        sudoku_valido = [
+            [5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9]
+        ]
+        self.assertTrue(es_valido_sudoku(sudoku_valido))
+    
+    def test_sudoku_invalido(self):
+        sudoku_invalido = [
+            [5,5,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9]
+        ]
+        self.assertFalse(es_valido_sudoku(sudoku_invalido))
+
+if __name__ == '__main__':
+    unittest.main()
+
